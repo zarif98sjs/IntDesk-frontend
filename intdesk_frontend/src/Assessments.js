36813b0dee423a57c9d8387ce69312b99fee7538
Mashiatmm <1705005@ugrad.cse.buet.ac.bd>
2022-07-21 20:04:44 +0600

    Added get request in assessment.js

--- intdesk_frontend/src/Assessments.js
+++ intdesk_frontend/src/Assessments.js
@@ -1,72 +1,7 @@
-
-
-// const dataSource = [
-//     { id: 1, title: "How to reverse a Linked List ?", name: 'Wasif', upvotes: 21, views: 21 },
-//     { id: 1, title: "Tips from the author of Blind 75", name: 'Ali', upvotes: 19, views: 21 },
-//     { id: 1, title: "Does Data Structures matter ?", name: 'Saad', upvotes: 16, views: 21 },
-//     { id: 1, title: "What is Agile software development ?", name: 'Asad', upvotes: 25, views: 21 },
-//     { id: 1, title: "How to reverse a Linked List ?", name: 'Wasif', upvotes: 21, views: 21 },
-//     { id: 1, title: "Tips from the author of Blind 75", name: 'Ali', upvotes: 19, views: 21 },
-//     { id: 1, title: "Does Data Structures matter ?", name: 'Saad', upvotes: 16, views: 21 },
-//     { id: 1, title: "What is Agile software development ?", name: 'Asad', upvotes: 25, views: 21 },
-//     { id: 1, title: "How to reverse a Linked List ?", name: 'Wasif', upvotes: 21, views: 21 },
-//     { id: 1, title: "Tips from the author of Blind 75", name: 'Ali', upvotes: 19, views: 21 },
-//     { id: 1, title: "Does Data Structures matter ?", name: 'Saad', upvotes: 16, views: 21 },
-//     { id: 1, title: "What is Agile software development ?", name: 'Asad', upvotes: 25, views: 21 },
-//     { id: 1, title: "How to reverse a Linked List ?", name: 'Wasif', upvotes: 21, views: 21 },
-//     { id: 1, title: "Tips from the author of Blind 75", name: 'Ali', upvotes: 19, views: 21 },
-//     { id: 1, title: "Does Data Structures matter ?", name: 'Saad', upvotes: 16, views: 21 },
-//     { id: 1, title: "What is Agile software development ?", name: 'Asad', upvotes: 25, views: 21 },
-//     { id: 1, title: "How to reverse a Linked List ?", name: 'Wasif', upvotes: 21, views: 21 },
-//     { id: 1, title: "Tips from the author of Blind 75", name: 'Ali', upvotes: 19, views: 21 },
-//     { id: 1, title: "Does Data Structures matter ?", name: 'Saad', upvotes: 16, views: 21 },
-//     { id: 1, title: "What is Agile software development ?", name: 'Asad', upvotes: 25, views: 21 },
-//   ];
-  
-//   const columns = [
-//     {
-//       title: 'Title',
-//       dataIndex: 'title',
-//       key: 'title',
-//       render: (_, record) => (
-//         <Space size="middle">
-//           <a href="/q">{record.title}</a>
-//           {/* <Link to="/App">{record.title}</Link> */}
-//         </Space>
-//       ),
-//     },
-//     {
-//       title: 'Posted By',
-//       dataIndex: 'name',
-//       key: 'name',
-//     },
-//     {
-//       title: 'Upvotes',
-//       dataIndex: 'upvotes',
-//       key: 'upvotes',
-//     },
-//     {
-//         title: 'Views',
-//         dataIndex: 'views',
-//         key: 'views',
-//       },
-//   ];
-
-//   function Assessments() {
-//     return (
-//             <div className="">
-//                 <Navbar />
-//                 <h1 id='title'>    All Questions </h1>
-//                <Table id='questions' dataSource={dataSource} columns={columns} />;
-//             </div>
-//           );
-//   }
-  
-// export default Assessments;
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Col, Row , Button} from 'antd';
 import { Navigate, Link } from 'react-router-dom';
+import axios from "axios";
 import Navbar from "./navbar";
 import './assess.css';
 
@@ -95,21 +30,41 @@ function Assessments() {
         { id: 14, image: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg", name: 'Python', takenBy: 21 },
     ];
 
+    const [assessments, setAssessments] = useState([]);
 
-    const element = flashcards.map(flashcard => (
+    // Extracting this method made it accessible for context/prop-drilling
+    const fetchAssessments = () => {
+          axios.get("http://localhost:8000/assessments/assessment/")
+          .then(res => {
+            console.log(window.$log = res.data);
+            const ara = res.data;
+            console.log(window.$log = ara);
+            setAssessments(ara);
+            })
+          .catch(err => {
+            console.log(err);
+          })
+      };
+
+      useEffect(() => {    
+        fetchAssessments();
+      }, []);
+
+    const element = assessments.map(assess_ind => (
         <Col span={6}>
-        <div key={flashcard.id}>
-            <Link to={`assess_ques/${flashcard.id}`}>
-            <Card className='site-card-wrapper' hoverable style={{ width: 240, height: 300}} cover={<img alt="example" className = 'photo' src={flashcard.image} />} >
-                <h2 style={{textAlign: "center"}}>{flashcard.name}</h2>
-                <h4>Taken By : {flashcard.takenBy}</h4>
+        <div key={assess_ind.id}>
+            <Link to={`assess_ques/${assess_ind.id}`}>
+            <Card className='site-card-wrapper' hoverable style={{ width: 240, height: 300}} cover={<img alt="example" className = 'photo' src={assess_ind.image_link} />} >
+                <h2 style={{textAlign: "center"}}>{assess_ind.skill_name}</h2>
+                <h4>Taken By : {assess_ind.taken_by}</h4>
+                <h4>Passed By : {assess_ind.passed_by}</h4>
                 <Button  type="link" htmlType="submit">Take Assessment Quiz </Button>
             </Card>
             </Link>
         </div>
         </Col>
     ))
-    
+
 
 
     return (
@@ -119,6 +74,7 @@ function Assessments() {
             <h1 id='title'> All Assessments </h1>
             <Row gutter={[16, 24]}>
               {element}
+              
             </Row>             
         </div>
     )
@@ -128,10 +84,5 @@ function Assessments() {
 
 export default Assessments;
 
-/* <Card hoverable style={{ width: 240,}} cover={<img alt="example" src={flashcard.image} />} >
-                            <h2 style={{textAlign: "center"}}>{flashcard.name}</h2>
-                            <Button type="primary" htmlType="submit">Take Assessment Quiz </Button>
-                        </Card> */
-
-                        // <div key={flashcard.id} onClick={goToDetails} role="button" tabIndex={0}>
-
+/* cover={<img alt="example" className = 'photo' src={flashcard.image} />} */
+                       
